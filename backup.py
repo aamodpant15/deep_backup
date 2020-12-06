ea676822cf2e31dfd9e747dc1f046287335382ae
Aamod Pant Balekundri <64889283+aamodpant15@users.noreply.github.com>
2020-12-06 20:59:59 +0530

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+
+# os.system("cp -v -R \"/Users/aamod/Aamod/UMass/\" \"/Volumes/HDD/Aamod/UMass/\"")
+def backup(origin, dest, verbose = False):
+    v = ""
+    if origin is None:
+        origin = "/Users/aamod/Aamod/UMass/"
+    if dest is None:
+        dest = "/Volumes/HDD/Aamod/UMass/"
+    if verbose:
+        v = "-v "
+    cmd = "cp "+v+"-R \"" + origin + "\" \"" + dest +"\""
+    os.system(cmd)
+
+def main():
+    parser = argparse.ArgumentParser(description='Deep copy folders')
+
+    parser.add_argument('-v',
+                       '--verbose',
+                       action='store_true',
+                       help='Output files while copying')
+
+    parser.add_argument('-origin', type = str,
+                        help ='Origin pathname with escape characters.',
+                        metavar = 'o')
+    parser.add_argument('-destination', type = str,
+                        help ='Destination pathname with escape characters.',
+                        metavar = 'd')
+
+    args = parser.parse_args()
+    print(args)
+    backup(args.origin, args.destination, args.verbose)
+
+if __name__ == "__main__":
+    main()
